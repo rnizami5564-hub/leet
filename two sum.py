b216f72b26cb7b65b1807c7149bb0a4d21caeeb2
rnizami5564-hub <rnizami5564@gmail.com>
2026-05-11 12:27:31 +0530

    Update two sum.py

--- two sum.py
+++ two sum.py
@@ -1,10 +1,9 @@
 def twoSum(nums, target):
     for i in range(len(nums)):
-        for j in range(i,len(nums)):
-            s=nums[i]+nums[j]
+        for j in range(i+1,len(nums)):
+            s= nums[i] + nums[j]
             if s==target:
              print(i,j,s)
-             break
 twoSum([10,4,1,5,3,6],10)
  
   
