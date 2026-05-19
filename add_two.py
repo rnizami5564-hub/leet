a7e404859370490d4e2360ea9becd9cc985c7b34
rnizami5564-hub <rnizami5564@gmail.com>
2026-05-19 21:42:11 +0530

    Rename add_two to add_two.py

---
+++ add_two.py
@@ -0,0 +1,21 @@
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next 
+class Solution(object):
+    def addTwoNumbers(self, l1, l2):
+        head=ListNode(0)
+        current=head
+        carry=0
+        while l1 or l2 or carry :
+            val1= l1.val if l1 else 0
+            val2= l2.val if l2 else 0
+            total = carry + val1 + val2
+            carry = total//10
+            current.next=ListNode(total%10)
+            current=current.next
+            if l1:
+                l1=l1.next
+            if l2:
+                l2=l2.next  
+        return head.next
