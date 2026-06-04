ab0fdb70b61e32aecc315a1e4a55975540c2f76a
rnizami5564-hub <rnizami5564@gmail.com>
2026-06-04 23:26:30 +0530

    6 ms (80.62%) 12.3 MB (54.3%) 0009-palindrome-number Easy06-04-2026

--- 0009-palindrome-number/0009-palindrome-number.py
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -1,7 +1,12 @@
 class Solution(object):
     def isPalindrome(self, x):
-        if str(x) == str(x)[::-1]:
-            return True
-        else:
-            return False 
+        if x<0:
+            return False
+        rev=0
+        num=x
+        while num>0:
+            d=num%10
+            rev= rev*10 + d
+            num//=10
+        return rev == x
         
