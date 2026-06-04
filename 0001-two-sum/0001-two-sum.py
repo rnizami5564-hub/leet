f6a84a688da4f1e2580906bb738c96aeb62d2c1b
rnizami5564-hub <rnizami5564@gmail.com>
2026-06-04 22:46:45 +0530

    1827 ms (9.53%) 19.9 MB (68.55%) 0001-two-sum Easy06-04-2026

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,13 +1,7 @@
-class Solution :
-    def twoSum(self ,nums , target) :
-        dict={}
-        n=len(nums)
-        for i in range(n):
-            dict[nums[i]]=i
-        for i in range(n):
-            c = target - nums[i]
-            if c in dict and dict[c] != i:
-                return[i,dict[c]]
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i in range(len(nums)) :
+            for j in range(i+1,len(nums)) :
+                if nums[i] + nums[j] == target :
+                    return[i,j]
         return[]
-     
-                                        
