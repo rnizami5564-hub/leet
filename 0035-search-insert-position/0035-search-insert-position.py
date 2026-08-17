5c6110b5624c51796857dd25d09e9897149c0247
rnizami5564-hub <rnizami5564@gmail.com>
2026-08-17 15:19:51 +0530

    0 ms (100%) 20 MB (11.48%) 0035-search-insert-position Easy08-17-2026

--- 0035-search-insert-position/0035-search-insert-position.py
+++ 0035-search-insert-position/0035-search-insert-position.py
@@ -1,12 +1,20 @@
 class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
-          for i in range(len(nums)):
-            if nums[i] == target :
-                return i 
-            elif target not in nums:
-                nums.append(target)
-                nums.sort()
-                for i in range(len(nums)):
-                    if nums[i] == target :
-                        return i 
+        n = len(nums)
+        l = 0
+        r = n-1
+        while l <= r:
+            mid = (l+r)//2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] > target:
+                r = mid - 1
+            else:
+                l = mid + 1
+        return l
+
+
+
+
+                
                 
